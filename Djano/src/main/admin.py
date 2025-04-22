d661d3fe8f9e7eb417fbe3c5872a3068ff2d55aa
Radhika <radmah1204@gmail.com>
2025-04-22 12:01:06 +0530

    Added the listing model and headers for the home page

--- Djano/src/main/admin.py
+++ Djano/src/main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from.models import Listing
+
+#optional step to customize the admin interface for the Listing model
+class ListingAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Listing, ListingAdmin)  # Register the Listing model with the admin site
 
 # Register your models here.
---
+++ Djano/src/main/utils.py
@@ -0,0 +1,8 @@
+def user_listing_path(instance, filename):
+    """
+    Function to define the upload path for user profile photos.
+    The uploaded file will be stored in MEDIA_ROOT/user_<id>/<filename>.
+    :param instance: The instance of the Profile model.
+    :param filename: The name of the uploaded file.(the name of the image eg : user_3/img1.jpeg)
+    """
+    return f'user_{instance.user.id}/listings/{filename}'
